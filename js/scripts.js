044b4c3a1e874b8d28ba3d740a0567113d314f03
Brit Wallace <britwallace888@gmail.com>
2021-11-07 15:54:18 -0800

    Will print array with spaces
    Co-authored-by: Julien Leonardo <julien.m.leonardo@gmail.com>
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,40 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     let favoriteArray = [];
-    const person1Input = $("input#favoriteThing1").val();
-    const person2Input = $("input#favoriteThing2").val();
-    const animalInput= $("input#favoriteThing3").val();
+    const favoriteIceCream = $("input#favoriteThing1").val();
+    const favoriteMusic = $("input#favoriteThing2").val();
+    const favoriteFood = $("input#favoriteThing3").val();
     
 
-    favoriteArray.push(person1Input);
-    favoriteArray.push(person2Input);
-    favoriteArray.push(animalInput);
+    favoriteArray.push(favoriteIceCream);
+    favoriteArray.push(favoriteMusic);
+    favoriteArray.push(favoriteFood);
     
-    console.log(favoriteArray)
+    $(".favoriteSummary").append(favoriteArray[0]);
+    $(".favoriteSummary").append(favoriteArray[1]);
+    $(".favoriteSummary").append(favoriteArray[2]);
+   
+
+  
+    const favoriteSummary = favoriteArray.join(" ");
+    
+    $(".favoriteSummary").append(favoriteSummary);
 
 
     event.preventDefault();
   });
 });
+/*
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
+*/
