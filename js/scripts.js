463277677329b75583453bc628dff0e88ba429ea
Brit Wallace <britwallace888@gmail.com>
2021-11-07 17:03:26 -0800

    Added space between output favorites
    Co-authored-by: Julien Leonardo <julien.m.leonardo@gmail.com>
    Co-authored-by: Branden Clauson <clauson.branden@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,28 @@ $(document).ready(function() {
     const favoriteIceCream = $("input#favoriteThing1").val();
     const favoriteMusic = $("input#favoriteThing2").val();
     const favoriteFood = $("input#favoriteThing3").val();
+    const favoriteBev = $("input#favoriteThing4").val();
     
 
     favoriteArray.push(favoriteIceCream);
     favoriteArray.push(favoriteMusic);
     favoriteArray.push(favoriteFood);
-    
+    favoriteArray.push(favoriteBev);
+
+    /*
     $(".favoriteSummary").append(favoriteArray[0]);
     $(".favoriteSummary").append(favoriteArray[1]);
     $(".favoriteSummary").append(favoriteArray[2]);
-   
+    */
 
-  
+   
+    console.log(favoriteArray);
+  //This gives the input from user spaces between the words.
     const favoriteSummary = favoriteArray.join(" ");
-    
+    //combined each class favorite array
     $(".favoriteSummary").append(favoriteSummary);
 
 
     event.preventDefault();
   });
 });
-/*
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".exclamation").append(exclamationInput);
-    $(".verb").append(verbInput);
-    $(".noun").append(nounInput);
-
-    $("#story").show();
-
-    event.preventDefault();
-  });
-});
-*/
